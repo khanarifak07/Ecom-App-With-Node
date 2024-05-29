19db2d373b0c8c45a82149c55ab64a61c0002b2e
Arif Khan <khanarifak07@gmail.com>
2024-05-29 16:20:44 +0530

    Api error and Api response Utils created

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,8 +1,9 @@
+import { ApiResponse } from "../utils/apiRespponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 //register user
 const registerUser = asyncHandler(async (req, res) => {
-  return res.json({ message: "Working" });
+  res.status(200).json(new ApiResponse(200, "Success"));
 });
 
 export { registerUser };
